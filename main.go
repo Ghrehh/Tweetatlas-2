e639de4cd6d3f8caed27057279be3ac327b5f5b8
Gerard Burns <gjburns9@hotmail.com>
2018-01-03 15:48:26 +0000

    work on web portion

--- main.go
+++ main.go
@@ -1,10 +1,52 @@
 package main
 
 import (
+	"net/http"
+	"os"
+	"log"
+	"strings"
+
 	"github.com/ghrehh/tweetatlas/twitterstream"
 	"github.com/ghrehh/tweetatlas/keys"
-	"fmt"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/gorilla/websocket"
+	"github.com/gorilla/handlers"
+  "github.com/gorilla/mux"
 )
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var RecentTweet *twitter.Tweet
+
+func tweets(w http.ResponseWriter, r *http.Request) {
+	var lastTweetId int64 = 0
+	c, err := upgrader.Upgrade(w, r, nil)
+
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	for {
+		if lastTweetId != RecentTweet.ID {
+			lastTweetId = RecentTweet.ID
+
+			err = c.WriteJSON(RecentTweet)
+
+			if err != nil {
+				log.Print("write:", err)
+				break
+			}
+		}
+	}
+
+	defer c.Close()
+}
 
 func main() {
 	twitterKeys := keys.Parse(keys.Load())
@@ -12,8 +54,23 @@ func main() {
 	filter := []string{"happy"}
 	stream, _ := twitterstream.FilterStream(&streamer, filter)
 
-	for {
-		tweet := <- stream.Messages
-		fmt.Printf("%+v\n", tweet)
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		RecentTweet = tweet
 	}
+
+	go demux.HandleChan(stream.Messages)
+
+	port := os.Getenv("PORT")
+	r := mux.NewRouter()
+	r.Headers("Content-Type", "application/json")
+	r.HandleFunc("/tweets", tweets)
+
+	if port != "" {
+		portStrings := []string{":", port}
+		http.ListenAndServe(strings.Join(portStrings, ""), handlers.CORS()(r))
+	} else {
+		http.ListenAndServe(":5555", handlers.CORS()(r))
+	}
+	
 }
