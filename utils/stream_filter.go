07fd46336f6e1e4130e1f86a9375084815dc85ff
Gerard Burns <gjburns9@hotmail.com>
2018-01-21 21:55:31 +0000

    add environment variable search phrases

--- utils/stream_filter.go
+++ utils/stream_filter.go
@@ -25,6 +25,18 @@ func parseStreamParams(data []byte) StreamParams {
 }
 
 func GetStreamFilter() []string {
+
+	// Attempt to get a search phrase from an environment variable
+	// Env var is passed as a JSON string
+	var search_phrases []string
+	search_phrases_json := os.Getenv("SEARCH_PHRASES")
+	err := json.Unmarshal([]byte(search_phrases_json), &search_phrases)
+
+	if search_phrases_json != "" && err == nil {
+		return search_phrases
+	}
+
+	// Attempt to get a search phrase from config file
 	data, err := ioutil.ReadFile("config/stream_params.json")
 
 	if err != nil {
